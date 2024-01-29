1c7612bba658e6055306d5c7ca4b733f2838c050
송진현 <steve15963@gmail.com>
2024-01-29 13:21:35 +0900

    클래스 학습

--- 클래스.py
+++ 클래스.py
@@ -10,33 +10,33 @@ print(id(data1))
 print(type(data1))
 
 class Car:
-    name = str()
-    speed = int()
-    position = int()
+    _name = str()
+    _speed = int()
+    _position = int()
     def __init__(self,name: str ="자동차 기본이름",speed : int = 0,position: int = 0,*args) -> None:
-        self.name = name
-        self.speed = speed
-        self.position = position
+        self._name = name
+        self._speed = speed
+        self._position = position
         print(args)
     def move(self):
-        print(self.position,"에서",self.speed,"의 속도로")
-        self.position += self.speed
-        print(self.position,"으로 움직입니다.")
+        print(self._position,"에서",self._speed,"의 속도로")
+        self._position += self._speed
+        print(self._position,"로 움직입니다.")
     def acc(self):
-        if self.speed >= 0:
-            self.speed = self.speed + 1
+        if self._speed >= 0:
+            self._speed = self._speed + 1
         else:
             print("역방향으로 가속할 수 없습니다.")
     def bre(self):
-        if self.speed > 0:
-            self.speed = self.speed - 1
-        elif self.speed < 0:
-            self.speed = self.speed + 1
+        if self._speed > 0:
+            self._speed = self._speed - 1
+        elif self._speed < 0:
+            self._speed = self._speed + 1
         else:
             print("정차하였습니다.")
     def revAcc(self):
-        if self.speed <= 0:
-            self.speed = self.speed - 1
+        if self._speed <= 0:
+            self._speed = self._speed - 1
         else:
             print("정방향으로 가속할 수 없습니다.")
 
@@ -45,3 +45,31 @@ myCar = Car("자동차",1,2,1,2)
 myCar.bre()
 myCar.acc()
 myCar.move()
+
+
+class SportsCar(Car):
+    def acc(self):
+        if self._speed >= 0:
+            self._speed = self._speed + 2
+        else:
+            print("역방향으로 가속할 수 없습니다.")
+    def bre(self):
+        if self._speed > 0:
+            self._speed = self._speed - 2
+        elif self._speed < 0:
+            self._speed = self._speed + 2
+        else:
+            print("정차하였습니다.")
+
+youCar = SportsCar()
+
+youCar.bre()
+youCar.acc()
+youCar.move()
+
+print("=" * 20)
+
+def testFun(data:Car):
+    data.move()
+print("다형성 테스트 : ")
+testFun( youCar)
